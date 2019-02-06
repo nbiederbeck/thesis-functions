36eab248aa21e6e4f56333e60f347ea36c069e3e
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-02-06 15:10:08 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thesis_functions",
+    description="",
+    version="0.1.0",
+    author="Noah Biederbeck",
+    author_email="noah.biederbeck@tu-dortmund.de",
+    packages=find_packages(),
+    install_requires=[],
+)

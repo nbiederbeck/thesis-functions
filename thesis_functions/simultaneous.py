eb8cf73a98170da4c78d7dfa8da49c427dd5e7e7
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-05-17 15:44:38 +0200

    calculate mean time from both timestamps

--- thesis_functions/simultaneous.py
+++ thesis_functions/simultaneous.py
@@ -24,6 +24,20 @@ def magic_times_to_unixtime(times):
     # Get MJD, directly converted to unixtime (-> seconds)
     mjd = Time(times[b"MTime_1.fMjd"].array(), format="mjd").unix
 
-    timestamps = mjd + s_f
+    timestamps1 = np.array((mjd + s_f) * 1e9)
+
+    # Get milliseconds (int), nanoseconds (int)
+    ms = times[b"MTime_2.fTime.fMilliSec"].array()
+    ns = times[b"MTime_2.fNanoSec"].array()
+
+    # Calculate seconds (float)
+    s_f = np.array(ms + ns / 1e6) / 1e3
+
+    # Get MJD, directly converted to unixtime (-> seconds)
+    mjd = Time(times[b"MTime_2.fMjd"].array(), format="mjd").unix
+
+    timestamps2 = np.array((mjd + s_f) * 1e9)
+
+    timestamps = (timestamps1 + timestamps2) / 2.0
 
     return timestamps
